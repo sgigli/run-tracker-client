c5d96152856b404108f24b88e5c4a0daecdaf372
sgigli <simon.luciano@gigli.net>
2019-12-12 17:18:38 -0500

    CRUD for runs working, user not notified yet though

--- assets/scripts/runs/api.js
+++ assets/scripts/runs/api.js
@@ -1,14 +1,53 @@
 'use strict'
 
 const config = require('../config')
+const store = require('../store')
 
 const index = () => {
   return $.ajax({
     url: config.apiUrl + '/runs',
-    method: 'GET'
+    method: 'GET',
+    headers: {
+      Authorization: `Token token=${store.user.token}`
+    }
+  })
+}
+
+const create = (data) => {
+  return $.ajax({
+    url: config.apiUrl + '/runs',
+    method: 'POST',
+    headers: {
+      Authorization: `Token token=${store.user.token}`
+    },
+    data
+  })
+}
+
+const update = (data) => {
+  return $.ajax({
+    url: config.apiUrl + '/runs/' + data.run.id,
+    method: 'PATCH',
+    headers: {
+      Authorization: `Token token=${store.user.token}`
+    },
+    data
+  })
+}
+
+const destroy = (data) => {
+  return $.ajax({
+    url: config.apiUrl + '/runs/' + data.run.id,
+    method: 'DELETE',
+    headers: {
+      Authorization: `Token token=${store.user.token}`
+    }
   })
 }
 
 module.exports = {
-  index
+  index,
+  create,
+  update,
+  destroy
 }
--- assets/scripts/runs/events.js
+++ assets/scripts/runs/events.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const runsApi = require('./api.js')
+const getFormFields = require('../../../lib/get-form-fields')
 // const runsUi = require('./ui.js');
 
 // get in the habit of naming your handlers, it eases debugging.
@@ -12,39 +13,44 @@ const onGetRuns = function (event) {
   event.preventDefault()
 
   runsApi.index()
-    .then(console.log('success'))
+    .then(console.log)
     .catch(console.error)
 }
 
-// const onDeleteRun = function(event) {
-//   event.preventDefault()
-//
-//   let data = getFormFields(event.target);
-//   booksApi.destroy(data.book.id)
-//     .then(booksUi.onDeleteSuccess)
-//     .catch(booksUi.onError)
-// };
-//
-// const onUpdateRun = function(event) {
-//   event.preventDefault()
-//
-//   let data = getFormFields(event.target);
-//   booksApi.update(data)
-//     .then(booksUi.onUpdateSuccess)
-//     .catch(booksUi.onError);
-// };
-//
-// const onCreateRun = function(event) {
-//   event.preventDefault()
-//
-//   let data = getFormFields(event.target);
-//   booksApi.create(data)
-//     .then(booksUi.onCreateSuccess)
-//     .catch(booksUi.onError)
-// }
+const onDeleteRun = function (event) {
+  event.preventDefault()
+
+  const data = getFormFields(event.target)
+  console.log(data)
+  runsApi.destroy(data)
+    .then(console.log)
+    .catch(console.error)
+}
+
+const onUpdateRun = function (event) {
+  event.preventDefault()
+
+  const data = getFormFields(event.target)
+  console.log(data)
+  runsApi.update(data)
+    .then(console.log)
+    .catch(console.error)
+}
+
+const onCreateRun = function (event) {
+  event.preventDefault()
+
+  const data = getFormFields(event.target)
+  runsApi.create(data)
+    .then(console.log)
+    .catch(console.error)
+}
 
 const addHandlers = () => {
   $('.get-runs').on('submit', onGetRuns)
+  $('.create-run').on('submit', onCreateRun)
+  $('.update-run').on('submit', onUpdateRun)
+  $('.delete-run').on('submit', onDeleteRun)
   // onDeleteRun()
   // onCreateRun()
   // onUpdateRun()
