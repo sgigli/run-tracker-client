1b45fa8fdb554155e4b1bc27d2a91a2cb62cbbdc
sgigli <simon.luciano@gigli.net>
2019-12-12 12:01:06 -0500

    web app auth working

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -1,5 +1,52 @@
-const addHandlers = {
+'use strict'
 
+const api = require('./api')
+const ui = require('./ui')
+const getFormFields = require('../../../lib/get-form-fields')
+
+const onSignUp = event => {
+  event.preventDefault()
+  const form = event.target
+  const formData = getFormFields(form)
+
+  api.signUp(formData)
+    .then(ui.onSignUpSuccess) // ui.onSignUpSuccess)
+    .catch(ui.onSignUpFailure) // ui.onSignUpFailure)
+}
+
+const onSignIn = event => {
+  event.preventDefault()
+  const form = event.target
+  const formData = getFormFields(form)
+
+  api.signIn(formData)
+    .then(ui.onSignInSuccess) // ui.onSignUpSuccess)
+    .catch(ui.onSignInFailure) // ui.onSignUpFailure)
+}
+
+const onSignOut = event => {
+  event.preventDefault()
+
+  api.signOut()
+    .then(ui.onSignOutSuccess)
+    .catch(ui.onSignOutFailure)
+}
+
+const onChangePassword = event => {
+  event.preventDefault()
+  const form = event.target
+  const formData = getFormFields(form)
+
+  api.changePassword(formData)
+    .then(ui.onChangePasswordSuccess)
+    .catch(ui.onChangePasswordFailure)
+}
+
+const addHandlers = () => {
+  $('#sign-up').on('submit', onSignUp)
+  $('#sign-in').on('submit', onSignIn)
+  $('#sign-out').on('submit', onSignOut)
+  $('#change-password').on('submit', onChangePassword)
 }
 
 module.exports = {
---
+++ assets/scripts/auth/ui.js
@@ -0,0 +1,65 @@
+'use strict'
+
+const store = require('../store')
+
+const onSuccess = message => {
+  $('#message').text(message)
+  $('form').trigger('reset')
+}
+
+const onFailure = message => {
+  $('#message').text(message)
+  $('form').trigger('reset')
+}
+
+const onSignUpSuccess = () => {
+  onSuccess('You successfully signed up, now sign in!')
+}
+
+const onSignUpFailure = () => {
+  onFailure('Please try again.')
+}
+
+const onSignInSuccess = responseData => {
+  store.user = responseData.user
+  // console.log(store)
+  $('#message').text('You successfully signed in!')
+  // $('.after-auth').show()
+  // $('.before-auth').hide()
+}
+
+const onSignInFailure = () => {
+  onFailure('Please try to sign in again')
+}
+
+const onSignOutSuccess = () => {
+  store.user = {}
+  onSuccess('You successfully signed out!')
+  // $('.before-auth').show()
+  // $('.after-auth').hide()
+}
+
+const onSignOutFailure = () => {
+  onFailure('Please try to sign out again.')
+}
+
+const onChangePasswordSuccess = () => {
+  $('form').trigger('reset')
+  $('#message').text('Successful password change!')
+}
+
+const onChangePasswordFailure = () => {
+  $('form').trigger('reset')
+  $('#message').text('Please try again')
+}
+
+module.exports = {
+  onSignUpSuccess,
+  onSignUpFailure,
+  onSignInSuccess,
+  onSignInFailure,
+  onSignOutSuccess,
+  onSignOutFailure,
+  onChangePasswordSuccess,
+  onChangePasswordFailure
+}
